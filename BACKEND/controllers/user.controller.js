185d3ccddf05f42c248287a33dbf344ef024bca7
ArpanSankesh <arpansankesh830@gmail.com>
2025-02-25 11:28:59 +0530

    /user/login

--- BACKEND/controllers/user.controller.js
+++ BACKEND/controllers/user.controller.js
@@ -23,3 +23,26 @@ module.exports.registerUser = async (req, res, next) => {
 
   res.status(201). json({ token, user});
 };
+
+module.exports.loginUser = async (req, res, next) => {
+  const error = validationResult(req);
+  if(!error.isEmpty()){
+    return res.status(400).json({errors: error.array()});
+  }
+
+  const {email, password} = req.body;
+
+  const user = await userService.findOne({email}).select('+password');
+  if(!user){
+    return res.status(402).json({message: 'Invalid Email or Password'});
+  }
+
+  const isMatch = await userModel.comparePassword(password, user.password);
+  if(!isMatch){
+    return res.status(402).json({message: 'Invalid Email or Password'});
+  }
+
+  const token = user.generateAuthToken();
+  res.status(200).json({token, user});
+  
+}
